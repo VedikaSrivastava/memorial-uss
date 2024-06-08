783a819f2f1fb3262da624da9735ef133417e498
VedikaSrivastava <vedikas3012@gmail.com>
2024-06-08 17:03:34 -0400

    some updated to style

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,8 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+            behavior: 'smooth',
+            block: 'start' 
         });
     });
 });
